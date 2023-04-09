d3bbd0eb45b939ec64f662423a572b270c37608c
Lars Grefer <github@larsgrefer.de>
2023-04-09 23:20:44 +0200

    V2

--- git-scan.sh
+++ git-scan.sh
@@ -1,21 +1,27 @@
 #!/usr/bin/env bash
 
+tmpDir=scan-tmp-dir
+
 git --version
 
-echo "Teste interne struktur"
+echo ""
+echo "## Teste interne Integrität des Repositories"
 git fsck --full --no-dangling
 
-echo "Entpacke alle blobs"
 echo ""
-mkdir scan-tmp
+echo "## Entpacke alle $(git cat-file --batch-all-objects --batch-check='%(objectname) %(objecttype)' | grep -c blob) Blobs nach $tmpDir"
+
 for object in $(git cat-file --batch-all-objects --batch-check='%(objectname) %(objecttype)' | grep blob | cut -d" " -f1,3-); do
-  echo -ne "Entpacke blob: $object\033[0K\r"
-  git cat-file -p $object > scan-tmp/$object.blob
+  echo -ne "Entpacke Blob: $object\033[0K\r"
+
+  mkdir -p $tmpDir/"${object:0:2}"
+  git cat-file -p $object > $tmpDir/"${object:0:2}"/"$object".blob
 done
 echo ""
 
-echo "Teste alle blobs"
+echo ""
+echo "## Teste alle Blobs mit ClamAV"
 
-clamscan --suppress-ok-results --archive-verbose --log=scan.log --recursive scan-tmp/
+clamscan --suppress-ok-results --verbose --archive-verbose --log=scan.log --recursive $tmpDir
 
-rm -r scan-tmp
+rm -r $tmpDir
