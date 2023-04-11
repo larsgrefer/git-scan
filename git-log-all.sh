58023048dc92fc6363876b81f343b624fb0f0da6
Lars Grefer <github@larsgrefer.de>
2023-04-11 12:03:04 +0200

    log all the commits --decorate

--- git-log-all.sh
+++ git-log-all.sh
@@ -3,7 +3,7 @@
 for gitDir in $(find . -type d | grep "\.git\$"); do
   echo "## Prüfe $gitDir"
 
-  git -C $gitDir log --all --date-order --graph --pretty=short
+  git -C $gitDir --no-pager log --all --date-order --graph --pretty=short --decorate
 
   echo ""
   echo ""
